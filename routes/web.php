eb02f92ab715967fe022e0eedcdc9527c6674e0a
Rahma Andita Purnama <purwantiibuku@gmail.com>
2024-10-27 14:29:12 +0700

    change default route login and root

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Filament\Facades\Filament;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+	return redirect()->to(Filament::getHomeUrl());
 });
+
+Route::get('/login', function () {
+	return redirect()->to(Filament::getLoginUrl());
+})->name('login');
